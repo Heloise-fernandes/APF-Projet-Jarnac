57f325fc35e8a3354ca29fc6114b9e3c79b54171
Maxime <maxime.chevalier@iut-valence.fr>
2015-12-20 21:25:36 +0100

    ajout commentaires dico.ml et dico.mli

--- Chevalier-Fernandes/Jarnac/dictionnaire.ml
+++ Chevalier-Fernandes/Jarnac/dictionnaire.ml
@@ -1,9 +1,22 @@
+
+(*Type permettant de representer un dictionnaire. Un noeud est un tableau compose d'un tableau a 26 case. Chaque case representant une
+	lettre (A=0,B=1....Z=25).*)
 type dico = Noeud of dico array * bool | Feuille
 
+(*Fonction permettant de creer un dictionnaire vide
+  Entree : unit
+  Sortie : Dictionnaire compose d'un neoud, chaque case du noeud etant compose d'une feuille*)
 let dico_vide () = Noeud((Array.make 26 Feuille),false)
 
+(*Fonction permettant de savoir l'indice d'une lettre dans l'alphabet (0 à 25)
+  Entree : un caractere
+  Sortie : l'indice du caractere dans l'alphabet*)
 let vallettre c = int_of_char c - int_of_char 'A'
 
+(*Fonction permettant de savoir si un mot (chaine de caractere) est presente dans un dictionnaire
+  Entrees : une chaine de caractere representant le mot a tester
+			le dictionnaire ou l'on doit chercher le mot
+  Sortie : Un boolen, true si le mot passe en parametre est dans le dico, faux sinon*)
 let member mot dictionnaire = 
 	let lgmot = String.length mot in
 	let rec aux caractere indice dic = if indice = lgmot-1 then (match dic with
@@ -20,13 +33,20 @@ let member mot dictionnaire =
 	aux premierelettre 0 dictionnaire
 
 
+(*Fonction permettant d'ajouter un mot dans un dictionnaire
+  Entrees : une chaine representant le mot
+			le dictionnaire dans lequel ajouter le mot
+  Sortie : le dictionnaire donne en parametre, connaissant le mot donne en parametre*)
 let add mot dic = 
+	(*On definit la longueur du mot a ajouter*)
 	let lgmot = String.length mot in
+	(*Fonction permettant d'ajouter la derniere lettre du mot dans le dico afin qu'il soit reconnu*)
 	let aux2 dico =( match dico with
 					  |Noeud(tab,b) -> Noeud(tab, true)
 					  |Feuille ->let nvlettre = dico_vide () in
 								(match nvlettre with
 								|Noeud(tab,b) ->Noeud(tab, true))) in
+	(*Fonction permettant d'ajouter recursivement chaque lettre du mot au dictionnaire*)
 	let rec aux caractere indice dic = 
 		if indice = lgmot-1 then (match dic with
 									|Noeud(tab,b)->begin (tab.(vallettre caractere) <- aux2 (tab.(vallettre caractere))) ;Noeud(tab,b) end
@@ -44,6 +64,10 @@ let add mot dic =
 	aux premierelettre 0 dic
 
 
+(*Fonction permettant de supprimer un mot dans un dictionnaire
+  Entrees : une chaine representant le mot
+			le dictionnaire dans lequel supprimer le mot
+  Sortie : le dictionnaire donne en parametre, ne connaissant plus le mot donne en parametre*)
 let remove mot dico =
   let lgmot = String.length mot in
   let dicovide = dico_vide () in
@@ -63,17 +87,24 @@ let remove mot dico =
 				|Feuille -> Feuille (*le mot n'existe pas*)
              ) in
   let premierelettre = String.get mot 0 in
-  (*Demander a Binome si on veut le plus petit dico possible (Une feuille) ou un dico_vide () quand on supprime *)
+  (*Si le dictionnaire est le dico vide, il sera compose seulement d'une feuille. On teste donc si c'est une feuille et on renvoit 
+    un dictionnaire vide comme declare dans la fonction dico_vide () le cas echeant *)
   let dicoarendre = aux premierelettre 0 dico in
   if dicoarendre = Feuille then dico_vide () else dicoarendre
 
 
-
+(*Fonction non traite*)
 (* val of_stream : char stream -> dico *)
 
-
+(*Fonction renvoyant un caractere a partir de son indice dans l'alphabet(0..25)
+  Entree : un entier
+  Sortie : un caractere *)
 let val_to_lettre i = char_of_int (int_of_char 'A' + i)
 
+
+(*Fonction permettant de creer une liste composee de tous les mot connu par un dictionnaire
+	  Entree : un dictionnaire dont on veut la liste des mots connus
+	  Sortie : la liste des mot connus par le dictionnaire*)
 let to_list dic = 
   let rec aux noeud indice debMot liste =
       if indice = 26 then liste
--- Chevalier-Fernandes/Jarnac/dictionnaire.mli
+++ Chevalier-Fernandes/Jarnac/dictionnaire.mli
@@ -1,14 +1,36 @@
 (*=============== Dictionnaire ===============*)
+
+	(*Type permettant de representer un dictionnaire. Un noeud est un tableau compose d'un tableau a 26 case. Chaque case representant une
+	lettre (A=0,B=1....Z=25).*)
 	type dico = Noeud of dico array * bool | Feuille
 	
+	(*Fonction permettant de creer un dictionnaire vide
+	  Entree : unit
+	  Sortie : Dictionnaire compose d'un neoud, chaque case du noeud etant compose d'une feuille*)
 	val dico_vide : unit -> dico
 	
+	(*Fonction permettant de savoir si un mot (chaine de caractere) est presente dans un dictionnaire
+	  Entrees : une chaine de caractere representant le mot a tester
+				le dictionnaire ou l'on doit chercher le mot
+	  Sortie : Un boolen, true si le mot passe en parametre est dans le dico, faux sinon*)
 	val member : string -> dico -> bool
 	
+	(*Fonction permettant d'ajouter un mot dans un dictionnaire
+	  Entrees : une chaine representant le mot
+				le dictionnaire dans lequel ajouter le mot
+	  Sortie : le dictionnaire donne en parametre, connaissant le mot donne en parametre*)
 	val add : string -> dico -> dico
 	
+	(*Fonction permettant de supprimer un mot dans un dictionnaire
+	  Entrees : une chaine representant le mot
+				le dictionnaire dans lequel supprimer le mot
+	  Sortie : le dictionnaire donne en parametre, ne connaissant plus le mot donne en parametre*)
 	val remove : string -> dico -> dico
 	
+	(*Fonction non traitee*)
 	(*val of_stream : char stream -> dico*)
 	
+	(*Fonction permettant de creer une liste composee de tous les mot connu par un dictionnaire
+	  Entree : un dictionnaire dont on veut la liste des mots connus
+	  Sortie : la liste des mot connus par le dictionnaire*)
 	val to_list : dico -> string list
